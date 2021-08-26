a07b9de80de7f2c8f1c99d9a48e3e2ce325e0bba
Aq-Zhou <87415838+Aq-Zhou@users.noreply.github.com>
2021-08-26 22:10:42 +0800

    Update frank.js

--- frank-com/public/frank.js
+++ frank-com/public/frank.js
@@ -1,4 +1,23 @@
-
+function ajax(method, url) {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open(method, url);
+      request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+          if (request.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(request);
+          }
+        }
+      };
+      request.send();
+    });
+  }
+  ajax("get", "http://qq.com:8888/friends.json").then(response => {
+    console.log("这是 AJAX");
+    console.log(response);
+  });
   function jsonp(url) {
     return new Promise((resolve, reject) => {
       const random = "frankJSONPCallbackName" + Math.random();
@@ -20,3 +39,4 @@
   jsonp("http://qq.com:8888/friends.js").then(data => {
     console.log(data);
   });
+  
